46bfe0078cb15b416f2242600a88aa4f6c157fa5
sheikhsuleman123 <mssheikh333@gmail.com>
2019-12-29 18:20:40 +0500

    basic header and footer ok

---
+++ blog/resources/views/Layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+   
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      
+      
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> msuleman.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
